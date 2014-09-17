48d9e503e01442e2779d113c35e4e42670c32286
minze121 <dizedong@gmail.com>
2014-09-17 13:49:51 +0800

    Change the greeter
    If the screen number can’t reach three, it will exit.

--- 03greeter_edan/main.cpp
+++ 03greeter_edan/main.cpp
@@ -15,50 +15,54 @@ int main(int argc,char * argv[])
     QDesktopWidget * desktopWidget = QApplication::desktop();
     int screenNum = desktopWidget->screenCount();
 
-    QRect rect;
-    int i;
-
-    Dialog window0;
-    Dialog window1(&window0);
-    Dialog window2(&window1);
-
-    qDebug()<<"window0 "<<window0.winId();
-    qDebug()<<"window1 "<<window1.winId();
-    qDebug()<<"window2 "<<window2.winId();
-    for (i=0;i<screenNum;i++)
+    if (screenNum == 3)
     {
-        rect = desktopWidget->screenGeometry(i);
-        qDebug()<<i<<" "<<rect;
+        QRect rect;
+        int i;
 
-        switch(rect.width())
-        {
-        case SCREEN0:
-        {
-            window0.move(rect.topLeft());
-            window0.resize(rect.width(),rect.height());
+        Dialog window0;
+        Dialog window1(&window0);
+        Dialog window2(&window1);
 
-            break;
-        }
-        case SCREEN1:
+        qDebug()<<"window0 "<<window0.winId();
+        qDebug()<<"window1 "<<window1.winId();
+        qDebug()<<"window2 "<<window2.winId();
+
+        for (i=0;i<screenNum;i++)
         {
-            window1.move(rect.topLeft());
-            window1.resize(rect.width(),rect.height());
-            break;
+            rect = desktopWidget->screenGeometry(i);
+            qDebug()<<i<<" "<<rect;
+
+            switch(rect.width())
+            {
+            case SCREEN0:
+            {
+                window0.move(rect.topLeft());
+                window0.resize(rect.width(),rect.height());
+
+                break;
+            }
+            case SCREEN1:
+            {
+                window1.move(rect.topLeft());
+                window1.resize(rect.width(),rect.height());
+                break;
+            }
+            case SCREEN2:
+            {
+                window2.move(rect.topLeft());
+                window2.resize(rect.width(),rect.height());
+                break;
+            }
+            }
         }
-        case SCREEN2:
-        {
-            window2.move(rect.topLeft());
-            window2.resize(rect.width(),rect.height());
-            break;
-        }
-        }
+
+        window0.show();
+        window1.show();
+        window2.show();
+
+        window2.startTimer();
+
+        return app.exec();
     }
-
-    window0.show();
-    window1.show();
-    window2.show();
-
-    window2.startTimer();
-
-    return app.exec();
 }
